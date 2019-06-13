a6565ba1efe8b06adc7c0d572ce07968c97807a7
Gavidof <sebastien.gavignet22@orange.fr>
2019-06-13 10:54:27 +0200

    RWFile

--- src/utilitary/RWFile.java
+++ src/utilitary/RWFile.java
@@ -1,14 +1,39 @@
 package utilitary;
 
+import java.io.*;
+
 public class RWFile {
 
 	/**
 	 *
 	 * @param fileName
 	 */
-	public void readFile(String fileName) {
+	public static ArrayList<String> readFile(String fileName) {
 		// TODO - implement RWFile.readFile
-		throw new UnsupportedOperationException();
+		ArrayList<String> ret = new ArrayList<String>();
+
+		if(fileName != null) {
+			try {
+				FileReader file = new FileReader(fileName);
+				BufferedReader in = new BufferedReader(file);
+
+				String s = in.readLine();
+
+				while(s != null) {
+					ret.add(s);
+					s = in.readLine();
+				}
+
+				in.close();
+			} catch(FileNotFoundException e) {
+				System.err.println("readFile : "+e.getMessage());
+			}
+		}
+		else {
+			System.err.println("readFile : fileName n'est pas valide.");
+		}
+
+		return ret;
 	}
 
 	/**
