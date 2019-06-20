e2cd5be63534cb1c7c26f894f1827318785f3eda
kernoeb <kernoeb@protonmail.com>
2019-06-20 23:56:11 +0200

    The player can move 🎉

--- src/controller/SquareEcouteur.java
+++ src/controller/SquareEcouteur.java
@@ -15,21 +15,27 @@ public class SquareEcouteur implements ActionListener {
   }
 
   public void actionPerformed(ActionEvent e) {
-    Square square = this.boardGUI.getSquare((JButton) e.getSource());
+  	Square square = this.boardGUI.getSquare((JButton) e.getSource());
 
+  		// System.out.println(this.boardGUI.getGame().getActualPlayer().getNbRestingFences());
 
-		if(this.actualPlayer.getNbRestingFences() > 0) {
+		// if (this.actualPlayer.getNbRestingFences() > 0) {
+		if (this.boardGUI.getGame().getActualPlayer().getNbRestingFences() > 0) {
 			if (square.isPawn()) {
-				this.actualPlayer.playPawn(square);
+				// this.actualPlayer.playPawn(square);
+				this.boardGUI.getGame().getActualPlayer().playPawn(square);
+				this.boardGUI.displayBoardGUI();
 			}
 
 			else if (square.isFence()) {
-				this.actualPlayer.playFence(square);
+				// this.actualPlayer.playFence(square);
+				this.boardGUI.getGame().getActualPlayer().playFence(square);
 				// TODO - Réactualiser le nombre de barrières en ayant accès à GameGUI
 			}
 		}
 		else {
-			this.actualPlayer.playPawn(square);
+			// this.actualPlayer.playPawn(square);
+			this.boardGUI.getGame().getActualPlayer().playPawn(square);
 		}
     
     // System.out.println("Le joueur a cliqué en : " + this.boardGUI.getX(square)/2 + ", " + this.boardGUI.getY(square)/2);
