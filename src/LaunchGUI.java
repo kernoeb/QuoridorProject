dba5a1bac150365ec382323ef773845b4e7056ae
Gavidof <sebastien.gavignet22@orange.fr>
2019-06-23 20:36:33 +0200

    Suppression classes inutiles

--- src/LaunchGUI.java
+++
@@ -1,9 +0,0 @@
-import quoridor.view.QuoridorGUI;
-
-class LaunchGUI {
-    public static void main(String[] args) {
-        System.setProperty("awt.useSystemAAFontSettings", "on");
-        System.setProperty("swing.aatext", "true");
-        javax.swing.SwingUtilities.invokeLater(QuoridorGUI::new);
-    }
-}
